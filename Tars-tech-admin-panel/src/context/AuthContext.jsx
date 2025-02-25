2eb2b123a29293471a0852440fc6399ed5a7f9ad
kshitij2201 <kshitijmeshram31@gmail.com>
2025-02-25 12:35:37 +0530

    added backend link in auth page

--- Tars-tech-admin-panel/src/context/AuthContext.jsx
+++ Tars-tech-admin-panel/src/context/AuthContext.jsx
@@ -34,7 +34,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://tars-tech-backend-chi.vercel.app/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username: email, password }),
